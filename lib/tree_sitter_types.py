b3e5fa939d328a53c0cdb27abfd7d310617155c8
Will Lillis <will.lillis24@gmail.com>
2025-08-14 03:02:35 -0400

    Array types working lets gooooooooo

--- lib/tree_sitter_types.py
+++ lib/tree_sitter_types.py
@@ -2,13 +2,14 @@
 from typing import List
 import re
 import lldb
+import lldb.formatters.Logger
+from lldb import (
+    SBValue,
+    # SBData,
+)
 
 
 class TreeSitterType(object):
-    # All caps type names with string definitions
-    # FOO = "Foo"
-    EMPTY = "Empty"
-    OTHER = "Other"
     # api.h
     # TODO: Move these type defs down once we find their actual definition files
     TS_PARSER = "TSParser"
@@ -59,7 +60,7 @@ class TreeSitterType(object):
     REUSABLE_NODE = "ReusableNode"
     # reduce_action.h
     REDUCE_ACTION = "ReduceAction"
-    REDUCE_ACTION_SET = "ReduceActionSet"
+    REDUCE_ACTION_SET = "ReduceActionSet" # Array(ReduceAction)
     # parser.h
     TS_FIELD_MAP_ENTRY = "TSFieldMapEntry"
     TS_MAP_SLICE = "TSMapSlice"
@@ -82,116 +83,71 @@ class TreeSitterType(object):
     LOOKAHEAD_ITERATOR = "LookaheadIterator"
     TS_RANGE_ARRAY = "TSRangeArray"
 
-
-    # # Rust examples:
-    # OTHER = "Other"
-    # STRUCT = "Struct"
-    # TUPLE = "Tuple"
-    # CSTYLE_VARIANT = "CStyleVariant"
-    # TUPLE_VARIANT = "TupleVariant"
-    # STRUCT_VARIANT = "StructVariant"
-    # ENUM = "Enum"
-    # EMPTY = "Empty"
-    # SINGLETON_ENUM = "SingletonEnum"
-    # REGULAR_ENUM = "RegularEnum"
-    # COMPRESSED_ENUM = "CompressedEnum"
-    # REGULAR_UNION = "RegularUnion"
-    #
-    # STD_STRING = "StdString"
-    # STD_OS_STRING = "StdOsString"
-    # STD_STR = "StdStr"
-    # STD_SLICE = "StdSlice"
-    # STD_VEC = "StdVec"
-    # STD_VEC_DEQUE = "StdVecDeque"
-    # STD_BTREE_SET = "StdBTreeSet"
-    # STD_BTREE_MAP = "StdBTreeMap"
-    # STD_HASH_MAP = "StdHashMap"
-    # STD_HASH_SET = "StdHashSet"
-    # STD_RC = "StdRc"
-    # STD_ARC = "StdArc"
-    # STD_CELL = "StdCell"
-    # STD_REF = "StdRef"
-    # STD_REF_MUT = "StdRefMut"
-    # STD_REF_CELL = "StdRefCell"
-    # STD_NONZERO_NUMBER = "StdNonZeroNumber"
-    # STD_PATH = "StdPath"
-    # STD_PATHBUF = "StdPathBuf"
-
 # Regexes for all the types
-# This is probably how the types are identified
-# TODO: See if we need to include an optional "struct" or "enum" before the type name
-TS_PARSER_REGEX = re.compile(r"^TSParser$")
-TS_TREE_REGEX = re.compile(r"^TSTree$")
-TS_QUERY_REGEX = re.compile(r"^TSQuery$")
-TS_QUERY_CURSOR_REGEX = re.compile(r"^TSQueryCursor$")
-TS_LOOKAHEAD_ITERATOR_REGEX = re.compile(r"^TSLookaheadIterator$")
-TS_INPUT_ENCODING_REGEX = re.compile(r"^TSInputEncoding$")
-TS_SYMBOL_TYPE_REGEX = re.compile(r"^TSSymbolType$")
-TS_POINT_REGEX = re.compile(r"^TSPoint$|^struct TSPoint$|^typedef TSPoint$")
-TS_RANGE_REGEX = re.compile(r"^TSRange$")
-TS_INPUT_REGEX = re.compile(r"^TSInput$")
-TS_PARSE_STATE_REGEX = re.compile(r"^TSParseState$")
-TS_LOG_TYPE_REGEX = re.compile(r"^TSLogType$")
-TS_LOGGER_REGEX = re.compile(r"^TSLogger$")
-TS_INPUT_EDITOR_REGEX = re.compile(r"^TSInputEdit$")
-TS_QUERY_CAPTURE_REGEX = re.compile(r"^TSQueryCapture$")
-TS_QUANTIFIER_REGEX = re.compile(r"^TSQuantifier$")
-TS_QUERY_MATCH_REGEX = re.compile(r"^TSQueryMatch$")
-TS_QUERY_PREDICATE_STEP_TYPE_REGEX = re.compile(r"^TSQueryPredicateStepType$")
-TS_QUERY_ERROR_REGEX = re.compile(r"^TSQueryError$")
-TS_QUERY_CURSOR_STATE_REGEX = re.compile(r"^TSQueryCursorState$")
-TS_QUERY_CURSOR_OPTIONS_REGEX = re.compile(r"^TSQueryCursorOptions$")
-TS_LANGUAGE_METADATA_REGEX = re.compile(r"^TSLanguageMetadata$")
-TS_WASM_ERROR_KIND_REGEX = re.compile(r"^TSWasmErrorKind$")
-TS_WASM_ERROR_REGEX = re.compile(r"^TSWasmError$")
-TREE_CURSOR_REGEX = re.compile(r"^TreeCursor$")
-TREE_CURSOR_STEP_REGEX = re.compile(r"^TreeCursorStep$")
-PARENT_CACHE_ENTRY_REGEX = re.compile(r"^ParentCacheEntry$")
-TS_TREE_REGEX = re.compile(r"^TSTree$")
-EXTERNAL_SCANNER_STATE_REGEX = re.compile(r"^ExternalScannerState$")
-SUBTREE_INLINE_DATA_REGEX = re.compile(r"^SubtreeInlineData$")
-SUBTREE_HEAP_DATA_REGEX = re.compile(r"^SubtreeHeapData$")
-SUBTREE_REGEX = re.compile(r"^Subtree$")
-MUTABLE_SUBTREE_REGEX = re.compile(r"^MutableSubtree$")
-SUBTREE_ARRAY_REGEX = re.compile(r"^SubtreeArray$")
-MUTABLE_SUBTREE_ARRAY_REGEX = re.compile(r"^MutableSubtreeArray$")
-SUBTREE_POOL_REGEX = re.compile(r"^SubtreePool$")
-STACK_SLICE_REGEX = re.compile(r"^StackSlice$")
-STACK_SLICE_ARRAY_REGEX = re.compile(r"^StackSliceArray$")
-STACK_SUMMARY_ENTRY_REGEX = re.compile(r"^StackSummaryEntry$")
-STACK_SUMMARY_REGEX = re.compile(r"^StackSummary$")
-STACK_ENTRY_REGEX = re.compile(r"^StackEntry$")
-REUSABLE_NODE_REGEX = re.compile(r"^ReusableNode$")
-REDUCE_ACTION_REGEX = re.compile(r"^ReduceAction$")
-REDUCE_ACTION_SET_REGEX = re.compile(r"^ReduceActionSet$")
-TS_FIELD_MAP_ENTRY_REGEX = re.compile(r"^TSFieldMapEntry$")
-TS_MAP_SLICE_REGEX = re.compile(r"^TSMapSlice$")
-TS_SYMBOL_METADATA_REGEX = re.compile(r"^TSSymbolMetadata$")
-TS_LEXER_REGEX = re.compile(r"^TSLexer$")
-TS_PARSE_ACTION_TYPE_REGEX = re.compile(r"^TSParseActionType$")
-TS_PARSE_ACTION_REGEX = re.compile(r"^TSParseAction$")
-TS_LEX_MODE_REGEX = re.compile(r"^TSLexMode$")
-TS_LEXER_MODE_REGEX = re.compile(r"^TSLexerMode$")
-TS_PARSE_ACTION_ENTRY_REGEX = re.compile(r"^TSParseActionEntry$")
-TS_CHARACTER_RANGE_REGEX = re.compile(r"^TSCharacterRange$")
-TS_LANGUAGE_REGEX = re.compile(r"^TSLanguage$")
-COLUMN_DATA_REGEX = re.compile(r"^ColumnData$")
-LEXER_REGEX = re.compile(r"^Lexer$")
-LENGTH_REGEX = re.compile(r"^Length$")
-TABLE_ENTRY_REGEX = re.compile(r"^TableEntry$")
-LOOKAHEAD_ITERATOR_REGEX = re.compile(r"^LookaheadIterator$")
-TS_RANGE_ARRAY_REGEX = re.compile(r"^TSRangeArray$")
-
-# These are used to distinguish between different types as designated by the debugger
-ENCODED_ENUM_PREFIX = "RUST$ENCODED$ENUM$" # TODO: See how C enums are encoded
-# TODO: Probably delete this
-#ENUM_DISR_FIELD_NAME = "<<variant>>"
-ENUM_LLDB_ENCODED_VARIANTS = "$variants$"
+#TS_PARSER_REGEX = r"^TSParser$|^struct TSParser$"
+TS_QUERY_REGEX = r"^TSQuery$|^struct TSQuery$|^typedef TSQuery$"
+TS_QUERY_CURSOR_REGEX = r"^TSQueryCursor$|^struct TSQueryCursor$|^typedef TSQueryCursor$"
+TS_LOOKAHEAD_ITERATOR_REGEX = r"^TSLookaheadIterator$|^struct TSLookaheadIterator$|^typedef TSLookaheadIterator$"
+TS_INPUT_ENCODING_REGEX = r"^TSInputEncoding$|^struct TSInputEncoding$|^typedef TSInputEncoding$"
+TS_SYMBOL_TYPE_REGEX = r"^TSSymbolType$|^struct TSSymbolType$|^typedef TSSymbolType$"
+TS_POINT_REGEX = r"^TSPoint$|^struct TSPoint$|^typedef TSPoint$"
+TS_RANGE_REGEX = r"^TSRange$|^struct TSRange$|^typedef TSRange$"
+TS_INPUT_REGEX = r"^TSInput$|^struct TSInput$|^typedef TSInput$"
+TS_PARSE_STATE_REGEX = r"^TSParseState$|^struct TSParseState$|^typedef TSParseState$"
+TS_LOG_TYPE_REGEX = r"^TSLogType$|^struct TSLogType$|^typedef TSLogType$"
+TS_LOGGER_REGEX = r"^TSLogger$|^struct TSLogger$|^typedef TSLogger$"
+TS_INPUT_EDITOR_REGEX = r"^TSInputEdit$|^struct TSInputEdit$|^typedef TSInputEdit$"
+TS_QUERY_CAPTURE_REGEX = r"^TSQueryCapture$|^struct TSQueryCapture$|^typedef TSQueryCapture$"
+TS_QUANTIFIER_REGEX = r"^TSQuantifier$|^struct TSQuantifier$|^typedef TSQuantifier$"
+TS_QUERY_MATCH_REGEX = r"^TSQueryMatch$|^struct TSQueryMatch$|^typedef TSQueryMatch$"
+TS_QUERY_PREDICATE_STEP_TYPE_REGEX = r"^TSQueryPredicateStepType$|^struct TSQueryPredicateStepType$|^typedef TSQueryPredicateStepType$"
+TS_QUERY_ERROR_REGEX = r"^TSQueryError$|^struct TSQueryError$|^typedef TSQueryError$"
+TS_QUERY_CURSOR_STATE_REGEX = r"^TSQueryCursorState$|^struct TSQueryCursorState$|^typedef TSQueryCursorState$"
+TS_QUERY_CURSOR_OPTIONS_REGEX = r"^TSQueryCursorOptions$|^struct TSQueryCursorOptions$|^typedef TSQueryCursorOptions$"
+TS_LANGUAGE_METADATA_REGEX = r"^TSLanguageMetadata$|^struct TSLanguageMetadata$|^typedef TSLanguageMetadata$"
+TS_WASM_ERROR_KIND_REGEX = r"^TSWasmErrorKind$|^struct TSWasmErrorKind$|^typedef TSWasmErrorKind$"
+TS_WASM_ERROR_REGEX = r"^TSWasmError$|^struct TSWasmError$|^typedef TSWasmError$"
+TREE_CURSOR_REGEX = r"^TreeCursor$|^struct TreeCursor$|^typedef TreeCursor$"
+TREE_CURSOR_STEP_REGEX = r"^TreeCursorStep$|^struct TreeCursorStep$|^typedef TreeCursorStep$"
+PARENT_CACHE_ENTRY_REGEX = r"^ParentCacheEntry$|^struct ParentCacheEntry$|^typedef ParentCacheEntry$"
+TS_TREE_REGEX = r"^TSTree$|^struct TSTree$|^typedef TSTree$"
+EXTERNAL_SCANNER_STATE_REGEX = r"^ExternalScannerState$|^struct ExternalScannerState$|^typedef ExternalScannerState$"
+SUBTREE_INLINE_DATA_REGEX = r"^SubtreeInlineData$|^struct SubtreeInlineData$|^typedef SubtreeInlineData$"
+SUBTREE_HEAP_DATA_REGEX = r"^SubtreeHeapData$|^struct SubtreeHeapData$|^typedef SubtreeHeapData$"
+SUBTREE_REGEX = r"^Subtree$|^struct Subtree$|^typedef Subtree$"
+MUTABLE_SUBTREE_REGEX = r"^MutableSubtree$|^struct MutableSubtree$|^typedef MutableSubtree$"
+SUBTREE_ARRAY_REGEX = r"^SubtreeArray$|^struct SubtreeArray$|^typedef SubtreeArray$"
+MUTABLE_SUBTREE_ARRAY_REGEX = r"^MutableSubtreeArray$|^struct MutableSubtreeArray$|^typedef MutableSubtreeArray$"
+SUBTREE_POOL_REGEX = r"^SubtreePool$|^struct SubtreePool$|^typedef SubtreePool$"
+STACK_SLICE_REGEX = r"^StackSlice$|^struct StackSlice$|^typedef StackSlice$"
+STACK_SLICE_ARRAY_REGEX = r"^StackSliceArray$|^struct StackSliceArray$|^typedef StackSliceArray$"
+STACK_SUMMARY_ENTRY_REGEX = r"^StackSummaryEntry$|^struct StackSummaryEntry$|^typedef StackSummaryEntry$"
+STACK_SUMMARY_REGEX = r"^StackSummary$|^struct StackSummary$|^typedef StackSummary$"
+STACK_ENTRY_REGEX = r"^StackEntry$|^struct StackEntry$|^typedef StackEntry$"
+REUSABLE_NODE_REGEX = r"^ReusableNode$|^struct ReusableNode$|^typedef ReusableNode$"
+REDUCE_ACTION_REGEX = r"^ReduceAction$|^struct ReduceAction$|^typedef ReduceAction$"
+REDUCE_ACTION_SET_REGEX = r"^ReduceActionSet$|^struct ReduceActionSet$|^typedef ReduceActionSet$"
+TS_FIELD_MAP_ENTRY_REGEX = r"^TSFieldMapEntry$|^struct TSFieldMapEntry$|^typedef TSFieldMapEntry$"
+TS_MAP_SLICE_REGEX = r"^TSMapSlice$|^struct TSMapSlice$|^typedef TSMapSlice$"
+TS_SYMBOL_METADATA_REGEX = r"^TSSymbolMetadata$|^struct TSSymbolMetadata$|^typedef TSSymbolMetadata$"
+TS_LEXER_REGEX = r"^TSLexer$|^struct TSLexer$|^typedef TSLexer$"
+TS_PARSE_ACTION_TYPE_REGEX = r"^TSParseActionType$|^struct TSParseActionType$|^typedef TSParseActionType$"
+TS_PARSE_ACTION_REGEX = r"^TSParseAction$|^struct TSParseAction$|^typedef TSParseAction$"
+TS_LEX_MODE_REGEX = r"^TSLexMode$|^struct TSLexMode$|^typedef TSLexMode$"
+TS_LEXER_MODE_REGEX = r"^TSLexerMode$|^struct TSLexerMode$|^typedef TSLexerMode$"
+TS_PARSE_ACTION_ENTRY_REGEX = r"^TSParseActionEntry$|^struct TSParseActionEntry$|^typedef TSParseActionEntry$"
+TS_CHARACTER_RANGE_REGEX = r"^TSCharacterRange$|^struct TSCharacterRange$|^typedef TSCharacterRange$"
+TS_LANGUAGE_REGEX = r"^TSLanguage$|^struct TSLanguage$|^typedef TSLanguage$"
+COLUMN_DATA_REGEX = r"^ColumnData$|^struct ColumnData$|^typedef ColumnData$"
+LEXER_REGEX = r"^Lexer$|^struct Lexer$|^typedef Lexer$"
+LENGTH_REGEX = r"^Length$|^struct Length$|^typedef Length$"
+TABLE_ENTRY_REGEX = r"^TableEntry$|^struct TableEntry$|^typedef TableEntry$"
+LOOKAHEAD_ITERATOR_REGEX = r"^LookaheadIterator$|^struct LookaheadIterator$|^typedef LookaheadIterator$"
+TS_RANGE_ARRAY_REGEX = r"^TSRangeArray$|^struct TSRangeArray$|^typedef TSRangeArray$"
 
 # Mapping of type defs to regexes
 TS_TYPE_TO_REGEX = {
-    TreeSitterType.TS_PARSER: TS_PARSER_REGEX,
-    TreeSitterType.TS_TREE: TS_TREE_REGEX,
+    #TreeSitterType.TS_PARSER: TS_PARSER_REGEX,
     TreeSitterType.TS_QUERY: TS_QUERY_REGEX,
     TreeSitterType.TS_QUERY_CURSOR: TS_QUERY_CURSOR_REGEX,
     TreeSitterType.TS_LOOKAHEAD_ITERATOR: TS_LOOKAHEAD_ITERATOR_REGEX,
@@ -315,7 +271,59 @@ class TSPointSyntheticProvider:
         # Optional: Recompute or refresh cached values if needed (e.g., if the underlying value changes).
         self.row = self.valobj.GetChildMemberWithName('row')  # Cache original fields for efficiency.
         self.column = self.valobj.GetChildMemberWithName('column')
-        pass
+
+# typedef Array(ReduceAction) ReduceActionSet;
+#
+# typedef struct {
+#   uint32_t count;
+#   TSSymbol symbol;
+#   int dynamic_precedence;
+#   unsigned short production_id;
+# } ReduceAction;
+#
+# define Array(T)      \
+# struct {             \
+#   T *contents;       \
+#   uint32_t size;     \
+#   uint32_t capacity; \
+# }
+
+
+class ReduceActionSetSyntheticProvider:
+    def __init__(self, valobj: SBValue, _dict):
+        # logger = Logger.Logger()
+        # logger >> "[StdVecSyntheticProvider] for " + str(valobj.GetName())
+        self.valobj = valobj
+        self.update()
+
+    def num_children(self) -> int:
+        return self.size
+
+    def get_child_index(self, name: str) -> int:
+        index = name.lstrip("[").rstrip("]")
+        if index.isdigit():
+            return int(index)
+        else:
+            return -1
+
+    def get_child_at_index(self, index: int) -> SBValue:
+        start = self.contents.GetValueAsUnsigned()
+        address = start + index * self.element_type_size
+        element = self.contents.CreateValueFromAddress(
+            "[%s]" % index, address, self.element_type
+        )
+        return element
+
+    def update(self):
+        self.contents = self.valobj.GetChildMemberWithName("contents") #.GetNonSyntheticValue()
+        self.size = self.valobj.GetChildMemberWithName("size").GetValueAsUnsigned()
+        self.capacity = self.valobj.GetChildMemberWithName("capacity").GetValueAsUnsigned()
+
+        self.element_type = self.contents.GetType().GetPointeeType()
+        self.element_type_size = self.element_type.GetByteSize()
+
+    def has_children(self) -> bool:
+        return True
 
 # Here's a simple summary function that uses the synthetic provider above
 def TSPointSummaryFromSynthetic(valobj, _dict) -> str:
@@ -335,17 +343,27 @@ def TSPointSummaryFromSynthetic(valobj, _dict) -> str:
 # self.first_element = self.valueObject.GetChildMemberWithName('values').GetChildMemberWithName('elements').GetChildMemberWithName('data')
 # https://github.com/jcredland/juce-toys/blob/master/juce_lldb_xcode.py
 #
-# This looks promising from our linked list instances: 
+# This looks promising for our linked list instances: 
 # https://yellowbrick.com/blog/yellowbrick-engineering/lldb-extension-for-structure-visualization/
 #
 # Look into using a commands file to source rather than the __lldb_init_module function as below
 
 def __lldb_init_module(debugger, internal_dict):
+    lldb.formatters.Logger._lldb_formatters_debug_level = 2
+    lldb.formatters.Logger._lldb_formatters_debug_filename = "/home/lillis/projects/tree-sitter/lldb-tree-sitter.log"
     # TODO: We probably want a smart way to iterate over all the types and register rather than
     # enumerating them all
     print("Registering TreeSitterTypes pretty printer...")
-    regex_str = r"^TSPoint$|^struct TSPoint$|^typedef TSPoint$"
-    debugger.HandleCommand(f"type synthetic add -l tree_sitter_types.TSPointSyntheticProvider -x '{regex_str}'")
+    # for type_name, type_regex in TS_TYPE_TO_REGEX.items():
+    #     # Register the type summary
+    #     debugger.HandleCommand(f"type summary add -F tree_sitter_types.{type_name}SummaryProvider -x '{type_regex}'")
+    #     # Register the synthetic provider
+    #     debugger.HandleCommand(f"type synthetic add -l tree_sitter_types.{type_name}SyntheticProvider -x '{type_regex}'")
+    # regex_str = r"^TSPoint$|^struct TSPoint$|^typedef TSPoint$"
+    # debugger.HandleCommand(f"type synthetic add -l tree_sitter_types.TSPointSyntheticProvider -x '{regex_str}'")
+    regex_str = TS_TYPE_TO_REGEX[TreeSitterType.REDUCE_ACTION_SET]
+    debugger.HandleCommand(f"type synthetic add -l tree_sitter_types.ReduceActionSetSyntheticProvider -x '{regex_str}'")
+    debugger.HandleCommand('type summary add -s "size=${svar%#}" ReduceActionSet')
     # debugger.HandleCommand(f"type summary add -F tree_sitter_types.TSPointSummaryFromSynthetic -x {regex_str}")
     print("TreeSitterTypes pretty printer registered")
 
