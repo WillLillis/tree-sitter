6e3e0a084ca2dcf7dafaf79d06715341e6de8953
Will Lillis <will.lillis24@gmail.com>
2025-08-30 15:59:10 -0400

    feat(test): pin fixture grammars to specific commits

--- crates/xtask/src/fetch.rs
+++ crates/xtask/src/fetch.rs
@@ -1,33 +1,24 @@
-use crate::{bail_on_err, root_dir, EMSCRIPTEN_VERSION};
+use crate::{bail_on_err, root_dir, FetchFixtures, EMSCRIPTEN_VERSION};
 use anyhow::Result;
-use std::process::Command;
+use std::{
+    fs,
+    process::{Command, Stdio},
+};
 
-pub fn run_fixtures() -> Result<()> {
-    let grammars_dir = root_dir().join("test").join("fixtures").join("grammars");
+pub fn run_fixtures(args: &FetchFixtures) -> Result<()> {
+    let fixtures_dir = root_dir().join("test").join("fixtures");
+    let grammars_dir = fixtures_dir.join("grammars");
+    let fixtures_path = fixtures_dir.join("fixtures.json");
 
-    [
-        ("bash", "master"),
-        ("c", "master"),
-        ("cpp", "master"),
-        ("embedded-template", "master"),
-        ("go", "master"),
-        ("html", "master"),
-        ("java", "master"),
-        ("javascript", "master"),
-        ("jsdoc", "master"),
-        ("json", "master"),
-        ("php", "master"),
-        ("python", "master"),
-        ("ruby", "master"),
-        ("rust", "master"),
-        ("typescript", "master"),
-    ]
-    .iter()
-    .try_for_each(|(grammar, r#ref)| {
-        let grammar_dir = grammars_dir.join(grammar);
+    // grammar name, branch, commit sha
+    let mut fixtures: Vec<(String, String, String)> =
+        serde_json::from_str(&fs::read_to_string(&fixtures_path)?)?;
+
+    for (grammar, branch, sha) in &mut fixtures {
+        let grammar_dir = grammars_dir.join(&grammar);
         let grammar_url = format!("https://github.com/tree-sitter/tree-sitter-{grammar}");
 
-        println!("Updating the {grammar} grammar...");
+        println!("Fetching the {grammar} grammar...");
 
         if !grammar_dir.exists() {
             let mut command = Command::new("git");
@@ -47,21 +38,44 @@ pub fn run_fixtures() -> Result<()> {
         std::env::set_current_dir(&grammar_dir)?;
 
         let mut command = Command::new("git");
-        command.args(["fetch", "origin", r#ref, "--depth", "1"]);
+        command.args(["fetch", "origin", sha, "--depth", "1"]);
         bail_on_err(
             &command.spawn()?.wait_with_output()?,
             &format!("Failed to fetch the {grammar} grammar"),
         )?;
 
         let mut command = Command::new("git");
-        command.args(["reset", "--hard", "FETCH_HEAD"]);
+        let reset_ref = if args.update {
+            format!("origin/{branch}")
+        } else {
+            sha.clone()
+        };
+        command.args(["reset", "--hard", &reset_ref]);
         bail_on_err(
             &command.spawn()?.wait_with_output()?,
             &format!("Failed to reset the {grammar} grammar"),
         )?;
+        if args.update {
+            let mut command = Command::new("git");
+            command.args(["rev-parse", "HEAD"]).stdout(Stdio::piped());
+            let update_out = command.spawn()?.wait_with_output()?;
+            bail_on_err(
+                &update_out,
+                &format!("Failed to parse the {grammar} grammar's latest commit"),
+            )?;
+            let new_sha = String::from_utf8(update_out.stdout)?.trim().to_string();
+            if !new_sha.eq(sha) {
+                println!("Updating the {grammar} grammar from {sha} to {new_sha}...");
+                *sha = new_sha;
+            }
+        }
+    }
 
-        Ok(())
-    })
+    if args.update {
+        fs::write(&fixtures_path, serde_json::to_string_pretty(&fixtures)?)?;
+    }
+
+    Ok(())
 }
 
 pub fn run_emscripten() -> Result<()> {
