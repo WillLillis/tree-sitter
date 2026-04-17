2361e62f1c0ee0742d5f2695976c7fa38a18d783
Will Lillis <will.lillis24@gmail.com>
2026-04-17 16:46:00 -0400

    cut down on LOC a bit

--- crates/generate/src/nativedsl/tests/errors/parse.rs
+++ crates/generate/src/nativedsl/tests/errors/parse.rs
@@ -1,56 +1,103 @@
 use super::super::*;
 
+macro_rules! parse_error_tests {
+    ($($name:ident { $input:expr, $expected:expr })*) => {
+        $(#[test] fn $name() {
+            let e = assert_err!(dsl_err($input), Parse);
+            assert_eq!(e.kind, $expected);
+        })*
+    };
+}
+
+parse_error_tests! {
+    error_unknown_grammar_field {
+        r#"grammar { language: "test", bogus: "x" } rule program { "x" }"#,
+        ParseErrorKind::UnknownGrammarField("bogus".into())
+    }
+    error_expected_token {
+        r#"grammar { language: "test" } rule program { seq("a" "b") }"#,
+        ParseErrorKind::ExpectedToken { expected: TokenKind::RParen, got: TokenKind::StringLit }
+    }
+    error_expected_expression {
+        r#"grammar { language: "test" } rule program { seq(,) }"#,
+        ParseErrorKind::ExpectedExpression
+    }
+    error_expected_item {
+        r#"grammar { language: "test" } "stray_string""#,
+        ParseErrorKind::ExpectedItem
+    }
+    error_unknown_type {
+        r#"grammar { language: "test" } let x: foo = "y" rule program { "x" }"#,
+        ParseErrorKind::UnknownType("foo".into())
+    }
+    error_missing_return_type {
+        r#"grammar { language: "test" } fn f(x: rule_t) { x } rule program { "x" }"#,
+        ParseErrorKind::MissingReturnType
+    }
+    error_multiple_grammar_blocks {
+        r#"grammar { language: "first" }
+        grammar { language: "second" }
+        rule program { "x" }"#,
+        ParseErrorKind::DuplicateGrammarBlock
+    }
+    error_expected_function_name {
+        r#"grammar { language: "test" }
+        let x = { a: 1 }
+        rule program { x.a("y") }"#,
+        ParseErrorKind::ExpectedFunctionName
+    }
+    error_expected_ident {
+        r#"grammar { language: "test" } rule { "x" }"#,
+        ParseErrorKind::ExpectedIdent
+    }
+    error_expected_string {
+        r#"grammar { language: 123 } rule program { "x" }"#,
+        ParseErrorKind::ExpectedString
+    }
+    error_expected_name {
+        r#"grammar { : "test" } rule program { "x" }"#,
+        ParseErrorKind::ExpectedName
+    }
+    error_expected_type {
+        r#"grammar { language: "test" }
+        let x: = "y"
+        rule program { "x" }"#,
+        ParseErrorKind::ExpectedType
+    }
+    error_nesting_too_deep {
+        // Constructed inline since it needs dynamic depth
+        &{
+            let deep = "(-".repeat(300) + "1" + &")".repeat(300);
+            format!("grammar {{ language: \"test\" }} rule foo {{ {deep} }}")
+        },
+        ParseErrorKind::NestingTooDeep
+    }
+}
+
 #[test]
-fn error_unknown_grammar_field() {
-    let err = dsl_err(
-        r#"
-        grammar { language: "test", bogus: "x" }
-        rule program { "x" }
-    "#,
+fn error_duplicate_grammar_field() {
+    let e = assert_err!(
+        dsl_err(
+            r#"grammar { language: "test", word: foo, word: bar }
+        rule foo { "x" } rule bar { "y" }"#
+        ),
+        Parse
     );
-    let e = assert_err!(err, Parse);
-    assert_eq!(e.kind, ParseErrorKind::UnknownGrammarField("bogus".into()));
+    assert_eq!(e.kind, ParseErrorKind::DuplicateGrammarField("word".into()));
+    assert!(e.note.is_some());
 }
 
 #[test]
-fn error_expected_token() {
-    let err = dsl_err(r#"grammar { language: "test" } rule program { seq("a" "b") }"#);
-    let e = assert_err!(err, Parse);
-    assert_eq!(
-        e.kind,
-        ParseErrorKind::ExpectedToken {
-            expected: TokenKind::RParen,
-            got: TokenKind::StringLit
-        }
+fn error_duplicate_object_key() {
+    let e = assert_err!(
+        dsl_err(
+            r#"grammar { language: "test" }
+        let x = { a: 1, b: 2, a: 3 } rule foo { "x" }"#
+        ),
+        Parse
     );
-}
-
-#[test]
-fn error_expected_expression() {
-    let err = dsl_err(r#"grammar { language: "test" } rule program { seq(,) }"#);
-    let e = assert_err!(err, Parse);
-    assert_eq!(e.kind, ParseErrorKind::ExpectedExpression);
-}
-
-#[test]
-fn error_expected_item() {
-    let err = dsl_err(r#"grammar { language: "test" } "stray_string""#);
-    let e = assert_err!(err, Parse);
-    assert_eq!(e.kind, ParseErrorKind::ExpectedItem);
-}
-
-#[test]
-fn error_unknown_type() {
-    let err = dsl_err(r#"grammar { language: "test" } let x: foo = "y" rule program { "x" }"#);
-    let e = assert_err!(err, Parse);
-    assert_eq!(e.kind, ParseErrorKind::UnknownType("foo".into()));
-}
-
-#[test]
-fn error_missing_return_type() {
-    let err = dsl_err(r#"grammar { language: "test" } fn f(x: rule_t) { x } rule program { "x" }"#);
-    let e = assert_err!(err, Parse);
-    assert_eq!(e.kind, ParseErrorKind::MissingReturnType);
+    assert_eq!(e.kind, ParseErrorKind::DuplicateObjectKey("a".into()));
+    assert!(e.note.is_some());
 }
 
 #[test]
@@ -61,24 +108,20 @@ fn error_builtin_arg_count_messages() {
         };
     }
     let cases: &[(&str, &str, u8, usize)] = &[
-        // 0-arg: blank
         (g!("blank(\"a\")"), "blank", 0, 1),
         (g!("blank(\"a\", \"b\")"), "blank", 0, 2),
-        // 1-arg: too few
         (g!("repeat()"), "repeat", 1, 0),
         (g!("repeat1()"), "repeat1", 1, 0),
         (g!("optional()"), "optional", 1, 0),
         (g!("token()"), "token", 1, 0),
         (g!("token_immediate()"), "token_immediate", 1, 0),
         (g!("inherit()"), "inherit", 1, 0),
-        // 1-arg: too many
         (g!(r#"repeat("a", "b")"#), "repeat", 1, 2),
         (g!(r#"repeat1("a", "b")"#), "repeat1", 1, 2),
         (g!(r#"optional("a", "b")"#), "optional", 1, 2),
         (g!(r#"token("a", "b")"#), "token", 1, 2),
         (g!(r#"token_immediate("a", "b")"#), "token_immediate", 1, 2),
         (g!(r#"inherit("a", "b")"#), "inherit", 1, 2),
-        // 2-arg: too few
         (g!("prec()"), "prec", 2, 0),
         (g!("prec(1)"), "prec", 2, 1),
         (g!("prec_left()"), "prec_left", 2, 0),
@@ -95,7 +138,6 @@ fn error_builtin_arg_count_messages() {
         (g!(r#"append("a")"#), "append", 2, 1),
         (g!("reserved()"), "reserved", 2, 0),
         (g!(r#"reserved("ctx")"#), "reserved", 2, 1),
-        // 2-arg: too many
         (g!(r#"prec(1, "a", "b")"#), "prec", 2, 3),
         (g!(r#"prec_left(1, "a", "b")"#), "prec_left", 2, 3),
         (g!(r#"prec_right(1, "a", "b")"#), "prec_right", 2, 3),
@@ -104,13 +146,11 @@ fn error_builtin_arg_count_messages() {
         (g!(r#"alias("a", "b", "c")"#), "alias", 2, 3),
         (g!(r#"append([1], [2], [3])"#), "append", 2, 3),
         (g!(r#"reserved("ctx", "a", "b")"#), "reserved", 2, 3),
-        // regexp (1-2 args)
         (g!("regexp()"), "regexp", 1, 0),
         (g!(r#"regexp("a", "b", "c")"#), "regexp", 2, 3),
     ];
     for &(src, name, expected, got) in cases {
-        let err = dsl_err(src);
-        let e = assert_err!(err, Parse);
+        let e = assert_err!(dsl_err(src), Parse);
         assert_eq!(
             e.kind,
             ParseErrorKind::WrongArgumentCount {
@@ -125,7 +165,7 @@ fn error_builtin_arg_count_messages() {
 
 #[test]
 fn error_builtin_arg_count_display() {
-    let cases = [
+    for (src, expected_msg) in [
         (
             r#"grammar { language: "test" } rule foo { prec(1) }"#,
             "'prec' takes 2 arguments, got 1",
@@ -138,17 +178,14 @@ fn error_builtin_arg_count_display() {
             r#"grammar { language: "test" } rule foo { blank("x") }"#,
             "'blank' takes no arguments, got 1",
         ),
-    ];
-    for (src, expected_msg) in cases {
-        let err = dsl_err(src);
-        let e = assert_err!(err, Parse);
+    ] {
+        let e = assert_err!(dsl_err(src), Parse);
         assert_eq!(e.to_string(), expected_msg, "wrong message for: {src}");
     }
 }
 
 #[test]
 fn keywords_as_identifiers() {
-    // Keywords are contextual - they are valid as rule/let/fn names.
     let g = dsl(r#"grammar { language: "test" } rule seq { "x" }"#);
     assert_eq!(g.variables[0].name, "seq");
     let g = dsl(r#"grammar { language: "test" } let token = "x" rule foo { token }"#);
@@ -160,66 +197,14 @@ fn keywords_as_identifiers() {
 }
 
 #[test]
-fn error_multiple_grammar_blocks() {
-    let err = dsl_err(
-        r#"
-        grammar { language: "first" }
-        grammar { language: "second" }
-        rule program { "x" }
-    "#,
-    );
-    let e = assert_err!(err, Parse);
-    assert_eq!(e.kind, ParseErrorKind::DuplicateGrammarBlock);
-}
-
-#[test]
-fn error_duplicate_grammar_field() {
-    let err = dsl_err(
-        r#"
-        grammar { language: "test", word: foo, word: bar }
-        rule foo { "x" }
-        rule bar { "y" }
-    "#,
-    );
-    let e = assert_err!(err, Parse);
-    assert_eq!(e.kind, ParseErrorKind::DuplicateGrammarField("word".into()));
-    assert!(e.note.is_some());
-}
-
-#[test]
-fn error_duplicate_object_key() {
-    let err = dsl_err(
-        r#"
-        grammar { language: "test" }
-        let x = { a: 1, b: 2, a: 3 }
-        rule foo { "x" }
-    "#,
-    );
-    let e = assert_err!(err, Parse);
-    assert_eq!(e.kind, ParseErrorKind::DuplicateObjectKey("a".into()));
-    assert!(e.note.is_some());
-}
-
-#[test]
-fn error_nesting_too_deep() {
-    let deep = "(-".repeat(300) + "1" + &")".repeat(300);
-    let src = format!("grammar {{ language: \"test\" }} rule foo {{ {deep} }}");
-    let err = dsl_err(&src);
-    let e = assert_err!(err, Parse);
-    assert_eq!(e.kind, ParseErrorKind::NestingTooDeep);
-}
-
-#[test]
 fn error_print_as_expression_rejected() {
-    // print() in expression position parses successfully (contextual keywords)
-    // but is rejected by the typechecker since it returns void_t.
-    let err = dsl_err(
-        r#"
-        grammar { language: "test" }
-        rule program { seq(print("hi"), "x") }
-    "#,
+    let e = assert_err!(
+        dsl_err(
+            r#"grammar { language: "test" }
+        rule program { seq(print("hi"), "x") }"#
+        ),
+        Type
     );
-    let e = assert_err!(err, Type);
     assert_eq!(
         e.kind,
         TypeErrorKind::TypeMismatch {
@@ -231,66 +216,14 @@ fn error_print_as_expression_rejected() {
 
 #[test]
 fn error_let_rhs_print_rejected() {
-    let err = dsl_err(
-        r#"
-        grammar { language: "test" }
-        let x = print("hi")
-        rule program { "x" }
-    "#,
+    let e = assert_err!(
+        dsl_err(
+            r#"grammar { language: "test" }
+        let x = print("hi") rule program { "x" }"#
+        ),
+        Type
     );
-    let e = assert_err!(err, Type);
     assert_eq!(e.kind, TypeErrorKind::CannotBindVoid);
-}
-
-#[test]
-fn error_expected_function_name() {
-    let err = dsl_err(
-        r#"
-        grammar { language: "test" }
-        let x = { a: 1 }
-        rule program { x.a("y") }
-    "#,
-    );
-    let e = assert_err!(err, Parse);
-    assert_eq!(e.kind, ParseErrorKind::ExpectedFunctionName);
-}
-
-#[test]
-fn error_expected_ident() {
-    // Missing rule name after `rule` keyword.
-    let err = dsl_err(r#"grammar { language: "test" } rule { "x" }"#);
-    let e = assert_err!(err, Parse);
-    assert_eq!(e.kind, ParseErrorKind::ExpectedIdent);
-}
-
-#[test]
-fn error_expected_string() {
-    // `language` expects a string literal, not an integer.
-    let err = dsl_err(r#"grammar { language: 123 } rule program { "x" }"#);
-    let e = assert_err!(err, Parse);
-    assert_eq!(e.kind, ParseErrorKind::ExpectedString);
-}
-
-#[test]
-fn error_expected_name() {
-    // Grammar field key must be a name, not a colon.
-    let err = dsl_err(r#"grammar { : "test" } rule program { "x" }"#);
-    let e = assert_err!(err, Parse);
-    assert_eq!(e.kind, ParseErrorKind::ExpectedName);
-}
-
-#[test]
-fn error_expected_type() {
-    // Type annotation position has an `=` instead of a type name.
-    let err = dsl_err(
-        r#"
-        grammar { language: "test" }
-        let x: = "y"
-        rule program { "x" }
-    "#,
-    );
-    let e = assert_err!(err, Parse);
-    assert_eq!(e.kind, ParseErrorKind::ExpectedType);
 }
 
 #[test]
@@ -299,18 +232,12 @@ fn error_inherited_parse_error() {
     let base_path = dir.path().join("base.tsg");
     std::fs::write(
         &base_path,
-        r#"grammar { language: "base" }
-rule program { seq("a" "b") }
-"#,
+        "grammar { language: \"base\" }\nrule program { seq(\"a\" \"b\") }\n",
     )
     .unwrap();
 
     let parent_path = dir.path().join("parent.tsg");
-    let parent_src = r#"let base = inherit("base.tsg")
-grammar { language: "derived", inherits: base }
-rule extra { "hello" }
-"#
-    .to_string();
+    let parent_src = "let base = inherit(\"base.tsg\")\ngrammar { language: \"derived\", inherits: base }\nrule extra { \"hello\" }\n".to_string();
     let err = parse_native_dsl(&parent_src, &parent_path).unwrap_err();
 
     let DslError::Module(inherited) = &err else {
